7f3b788ef1e7bd8df29b5ef79595a53630874210
Roger Xavier <rogerxavier10@gmail.com>
2025-06-17 16:15:07 -0300

    fix: header logo fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PedidosComponent } from './views/pedidos/pedidos.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: '',
+  // },
+  {
+    path: "pedidos",
+    // component: PedidosComponent,
+    children: [{
+      path: "", component: PedidosComponent
+    }]
+  },
+];
